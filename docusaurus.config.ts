c62db459f951f7af27322fab18d4482a7056c4cb
Clément Juventin <clementjuventin@gmail.com>
2025-06-06 16:13:18 +0200

    deploy setup

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,35 +2,23 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Clément Juventin',
   tagline: 'Blog and personal website',
-  // favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
-  url: 'https://clementjuventin.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://clementjuventin.github.io',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'clementjuventin', // Usually your GitHub org/user name.
-  projectName: 'personnal_website', // Usually your repo name.
+  organizationName: 'clementjuventin',
+  projectName: 'clementjuventin-blog',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -42,7 +30,7 @@ const config: Config = {
       {
         docs: false,
         blog: {
-          routeBasePath: '/', // 👈 Make blog the homepage
+          routeBasePath: '/', 
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
@@ -60,8 +48,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Clément Juventin',
       items: [
